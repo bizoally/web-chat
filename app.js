0353b808d46f1a1444c8521e20bc06e469d4e1e8
BizOAlly <93839871+bizoally@users.noreply.github.com>
2022-08-28 02:35:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-setCookie("name", "aniketcoockie", 30);
-var myDataRef = firebase.database().ref('chat');
-$('#namesubmit').click(() =>{
-  
-  var name = $('#nameInput').val();
-          if (name != null){
-    
-            $('.name').addClass("hide");
-            $('#namesubmit').addClass("hide");
-            $('#messageInput').removeClass("hide");
-            $('#msgsubmit').removeClass("hide");
-          }
 
-        });
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("name");
+  if (user != "") {
+    $('.name').addClass("hide");
+    $('#namesubmit').addClass("hide");
+    $('#messageInput').removeClass("hide");
+    $('#msgsubmit').removeClass("hide");
+  } else {
+    $('#namesubmit').click(() =>{
+  
+      var name = $('#nameInput').val();
+              if (name != null){
+                setCookie("name", name, 30);
+                $('.name').addClass("hide");
+                $('#namesubmit').addClass("hide");
+                $('#messageInput').removeClass("hide");
+                $('#msgsubmit').removeClass("hide");
+              }
+    
+            });
+  }
+}
+
+
+
+
+var myDataRef = firebase.database().ref('chat');
+
        
           $('#msgsubmit').click(()=> {
             if ($('#messageInput').val().length >=1){
